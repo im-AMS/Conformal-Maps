334ccf05fa30b85a284a18618b26b69b01ca1042
zolabar <82505312+zolabar@users.noreply.github.com>
2021-08-23 20:01:38 +0200

    details

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = '1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
