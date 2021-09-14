83acc86831302b538d1228d78cfdba36e619632b
zolabar <82505312+zolabar@users.noreply.github.com>
2021-09-14 11:56:13 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,9 +59,15 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 #html_theme = 'classic'
 #html_theme = 'nature'
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    #'logo_only': True,
+    'navigation_depth': 5,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
